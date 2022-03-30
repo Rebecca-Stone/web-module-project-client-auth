884efad1d28425d66669f0fbb4133ed51251029f
Rebecca-Stone <primstone16@gmail.com>
2022-03-29 20:13:17 -0500

    put everything in seperate component folders

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
-
-const Login = () => {
-  return <h2>Login</h2>;
-};
-
-const FriendsList = () => {
-  return <h2>Friends List</h2>;
-};
-
-const AddFriends = () => {
-  return <h2>Add Friends</h2>;
-};
+import Login from "./components/Login";
+import FriendsList from "./components/FriendsList";
+import AddFriends from "./components/AddFriends";
 
 function App() {
   return (
