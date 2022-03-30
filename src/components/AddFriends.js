ee518519d0d0a55eeaf4d3c8e49de00fa7d3f13e
Rebecca-Stone <primstone16@gmail.com>
2022-03-29 20:23:49 -0500

    fleshed out each component

--- src/components/AddFriends.js
+++ src/components/AddFriends.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 const AddFriends = () => {
-  return <h2>Add Friends</h2>;
+  return (
+    <div>
+      <h2>Add Friends</h2>
+      <form>
+        <div>
+          <label htmlFor="Username">Username: </label>
+          <input id="username" />
+        </div>
+        <div>
+          <label htmlFor="age">Age: </label>
+          <input id="age" />
+        </div>
+        <div>
+          <label htmlFor="Email">Email: </label>
+          <input id="email" />
+        </div>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default AddFriends;
